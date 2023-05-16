f00e5f8978caaefb8cafda8b5c173d720ad0df07
helpfin-cn <pawan.cybernext@gmail.com>
2023-05-16 14:54:51 +0530

    0.0.2: Cleanup index file

--- index.php
+++ index.php
@@ -8,21 +8,10 @@ $json = file_get_contents(__DIR__ . '/products.json');
 $data = json_decode($json, true);
 $collection = new Collection($data);
 
-echo "<p><b>Total sum of prices in pounds GBP:</b></br>";
-echo $total_price = '£'.$collection->sum('price_in_pence');
-echo "</p>";
+echo "<p><b>Total sum of prices in pounds GBP:</b></br> £".$collection->sum('price_in_pence')."</p>";
 
+echo "<p><b>Total sum of prices in pounds GBP for phones and laptops:</b></br> £".$collection->whereIn('category', ['Phone','Laptop'])->sum('price_in_pence')."</p>";
 
-echo "<p><b>Total sum of prices in pounds GBP for phones and laptops:</b></br>";
-echo $filtered = '£'.$collection->whereIn('category', ['Phone','Laptop'])->sum('price_in_pence');
-echo "</p>";
+echo "<p><b>Total count of graphics cards:</b></br>". $collection->whereIn('category', 'Graphics Card')->count(). "</p>";
 
-
-echo "<p><b>Total count of graphics cards:</b></br>";
-echo $count_graphics_cards = $collection->whereIn('category', 'Graphics Card')->count();
-echo "</p>";
-
-
-echo "<p><b>Comma separated list of the names of phones:</b></br>";
-echo $filtered_phones = $collection->whereIn('category', 'Phone')->implode('name', ', ');
-echo "</p>";
+echo "<p><b>Comma separated list of the names of phones:</b></br>". $collection->whereIn('category', 'Phone')->implode('name', ', '). "</p>";
